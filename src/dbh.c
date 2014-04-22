b6747b3fb5782ec8f9b864537fe0aabef3c4b207
Tomas Libal <tomas@libal.eu>
2014-04-22 21:07:03 +0200

    Changed the mongo-c-driver to 0.8x.x and added query test functions
    Changed the code to work with the older mongodb driver API and added two functions that connect to the db and perform testing operations

--- src/dbh.c
+++ src/dbh.c
@@ -1,15 +1,73 @@
 #include "dbh.h"
+#include "mongo.h"
 
-mongoc_database_t *database;
-mongoc_cursor_t   *cursor;
-mongoc_client_t   *client;
-const bson_t      *reply;
 uint16_t          port;
-bson_error_t      error;
-bson_t            ping;
 char              *host_and_port;
 char              *str;
 int               init = 0;
+
+static void test_empty_query( mongo *conn ) {
+    mongo_cursor cursor[1];
+    mongo_cursor_init( cursor, conn, "ua_detection.ua_strings" );
+
+    while( mongo_cursor_next( cursor ) == MONGO_OK ) {
+        bson_iterator iterator[1];
+
+        if ( bson_find(iterator, mongo_cursor_bson(cursor), "ua") ) {
+            printf("User-Agent: %s\n", bson_iterator_string(iterator));
+        }
+    }
+
+    mongo_cursor_destroy( cursor );
+}
+
+static void test_query( mongo *conn ) {
+    bson query[1];
+    mongo_cursor cursor[1];
+
+    bson_init( query );
+
+    bson_append_start_object( query, "count" );
+        bson_append_int( query, "$gt", 3);
+    bson_append_finish_object( query );
+
+    bson_finish( query );
+
+    mongo_cursor_init( cursor, conn, "ua_detection.keywords" );
+    mongo_cursor_set_query( cursor, query );
+
+    while( mongo_cursor_next( cursor ) == MONGO_OK ) {
+        bson_iterator keyword[1];
+        bson_iterator cnt[1];
+        if ( bson_find(keyword, mongo_cursor_bson(cursor), "value") &&
+        bson_find(cnt, mongo_cursor_bson(cursor), "count") ) {
+            printf("User-Agent: %s, count: %d\n", bson_iterator_string(keyword), bson_iterator_int(cnt));
+        }
+    }
+
+    bson_destroy( query );
+    mongo_cursor_destroy( cursor );
+}
+
+int dbh_test()
+{
+    mongo conn[1];
+    int status = mongo_client( conn, "127.0.01", 27017 );
+
+    if (status != MONGO_OK) {
+        switch ( conn->err ) {
+            case MONGO_CONN_NO_SOCKET:  printf("no socket\n"); return 1;
+            case MONGO_CONN_FAIL:       printf("connection failed\n"); return 1;
+            case MONGO_CONN_NOT_MASTER: printf("not master\n"); return 1;
+        }
+    }
+
+    //test_empty_query(conn);
+    //test_query(conn);
+
+    mongo_destroy(conn);
+    return 0;
+}
 
 int dbh_init(uint16_t *port)
 {
@@ -45,45 +103,8 @@ int dbh_init(uint16_t *port)
   }
   fclose(config_file);
 
-  //*host_and_port = bson_strdup_printf("mongodb://root:root@localhost:%du", port);
-  //host_and_port = bson_strdup("mongodb://root:root@localhost:27017");
-
   free(param);
   free(val);
-
-  return 0;
-}
-
-int dbh_test()
-{
-  if (init == 0)
-    dbh_init(&port);
-
-  // mongoc_init();
-  // client = mongoc_client_new(host_and_port);
-  // if(!client) {
-  //   fprintf(stderr, "Invalid hostname or port: %s\n", host_and_port);
-  //   return 2;
-  // }
-
-  // bson_init(&ping);
-  // bson_append_int32(&ping, "ping", 4, 1);
-  // database = mongoc_client_get_database(client, "ua_detection");
-  // cursor = mongoc_database_commnad(database, 0, 0, 1, 0, &ping, NULL, NULL);
-  // if (mongoc_cursor_next(cursor, &reply)) {
-  //   str = bson_as_json(reply, NULL);
-  //   fprintf(stdout, "%s\n", str);
-  //   bson_free(str);
-  // } else if (mongoc_cursor_error(cursor, &error)) {
-  //   fprintf(stderr, "Ping failure: %s\n", error.message);
-  //   return 3;
-  // }
-  //
-  // mongoc_cursor_destroy(cursor);
-  // bson_destroy(&ping);
-  // mongoc_client_destroy(client);
-  // bson_free(host_and_port);
-  //
 
   return 0;
 }
--- src/dbh.h
+++ src/dbh.h
@@ -1,9 +1,13 @@
 #ifndef DBH_HEADER
 #define DBH_HEADER
 
-#include <mongoc.h>
+#include "mongo.h"
 #include <stdio.h>
 
+static void test_query( mongo *conn );
+static void test_empty_query(mongo *conn);
+
 int dbh_init(uint16_t *port);
+int dbh_test();
 
 #endif
