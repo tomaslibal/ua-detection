5d4a9dceed75034bd8cc0637635579d4945bd1e8
Tomas Libal <tomas@libal.eu>
2014-04-22 21:25:55 +0200

    added 'static mongo* dbh_get_conn()'

--- src/dbh.c
+++ src/dbh.c
@@ -69,6 +69,22 @@ int dbh_test()
     return 0;
 }
 
+static mongo* dbh_get_conn()
+{
+    static mongo conn[1];
+    int status = mongo_client( conn, "127.0.0.1", 27017 );
+
+    if (status != MONGO_OK) {
+        switch ( conn->err ) {
+            case MONGO_CONN_NO_SOCKET:  printf("no socket\n"); return 1;
+            case MONGO_CONN_FAIL:       printf("connection failed\n"); return 1;
+            case MONGO_CONN_NOT_MASTER: printf("not master\n"); return 1;
+        }
+    }
+
+    return mongo;
+}
+
 int dbh_init(uint16_t *port)
 {
   char line[80];
