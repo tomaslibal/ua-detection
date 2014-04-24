d08b1caca2941cdb69c1200f818d15c953fa7107
Tomas Libal <tomas@libal.eu>
2014-04-24 20:00:29 +0200

    Implemented the initialization of the training set from the database, removed commented out code

--- src/ann.c
+++ src/ann.c
@@ -1,3 +1,14 @@
+//
+// Artificial Neural Network
+// A          N      N
+//
+// So far this is just a single perceptron
+// But depending on the need or if needed to achieve better results it may
+// change into a multi-layer neural network with eventual other features to
+// get better results.
+//
+// ann.c
+
 #include <string.h>
 #include <stdbool.h>
 #include <stdlib.h>
@@ -10,29 +21,16 @@ bool training_set_inited   = false;
 
 unsigned int iter          = 0; // number of iterations in each epoch
 unsigned int epochs        = 0; // number of epochs
-unsigned int MAX_EPOCHS    = 100;
+unsigned int MAX_EPOCHS    = 10;
 
 float t           = 0.5; // treshold
 double rate       = 0.1; // learnind rate;
 double weights[3] = { 0.0, 0.0, 0.0 }; // weights
 
-//trainingSetItem trainingSet[];
-
 // Once trained the output is saved to a data file so that the ANN doesn't
 // have to train every time but only when the training set is updated or when
 // the training is desired.
 char training_output_filename[] = "../data/to.dat";
-
-//
-// Artificial Neural Network
-// A          N      N
-//
-// So far this is just a single perceptron
-// But depending on the need or if needed to achieve better results it may
-// change into a multi-layer neural network with eventual other features to
-// get better results.
-//
-// ann.c
 
 // Calculates the dot product of two vectors
 // Precondition:
@@ -63,30 +61,66 @@ double dot_product(double *values, double *weights, unsigned int len)
 TrainingSetItem *load_training_set_from_db(unsigned int *plen)
 {
     // Get the number of documents-trainingSetItems from the mongodb
-    unsigned int cnt;
-    cnt = 4; // mock value
-
-    *plen = cnt;
+    char *coll;
+    coll = malloc(13);
+    strcpy(coll, "training_set");
+    *plen = get_doc_cnt(coll);
 
     static TrainingSetItem *p_internal;
 
-    p_internal = malloc(sizeof(TrainingSetItem)*cnt);
+    p_internal = malloc(sizeof(TrainingSetItem)*(*plen));
     if(p_internal == NULL) {
         perror("Error allocating memory");
         exit(1);
     }
 
-    // Initialize the training set
-    // MOCK VALUES FOR A NAND OPERATION TO SEE IF THE PERCEPTRON CAN LEARN
-    p_internal[0] = (TrainingSetItem){ .input_vector = { 1.0, 0.0, 0.0 }, .expected_output = 1 };
-    p_internal[1] = (TrainingSetItem){ .input_vector = { 1.0, 0.0, 1.0 }, .expected_output = 1 };
-    p_internal[2] = (TrainingSetItem){ .input_vector = { 1.0, 1.0, 0.0 }, .expected_output = 1 };
-    p_internal[3] = (TrainingSetItem){ .input_vector = { 1.0, 1.0, 1.0 }, .expected_output = 0 };
-    // ...
-    //memcpy(p_internal, ts, sizeof(TrainingSetItem)*cnt);
+    unsigned int i = 0;
+
+    mongo *conn = dbh_get_conn();
+    // query is empty because we want every document from the collection
+    bson query[1];
+    mongo_cursor cursor[1];
+
+    bson_init(query);
+    // empty query -eq {}
+    bson_finish(query);
+
+    mongo_cursor_init(cursor, conn, "ua_detection.training_set");
+    mongo_cursor_set_query(cursor, query);
+
+    while(mongo_cursor_next(cursor) == MONGO_OK) {
+        bson_iterator input_vector[1];
+        bson_iterator expected_output[1];
+
+        if (bson_find(input_vector, mongo_cursor_bson(cursor), "input_vector") &&
+        bson_find(expected_output, mongo_cursor_bson(cursor), "expected_output")) {
+            printf("Expected output = %d\n", bson_iterator_int(expected_output));
+
+            bson_iterator sub[1];
+
+            double tmp[3];
+            unsigned int j = 0;
+            bson_iterator_subiterator(input_vector, sub);
+
+            // init the input_vector values one by one
+            while(bson_iterator_more(sub)) {
+                if(bson_iterator_next(sub)!=BSON_EOO) {
+                    tmp[j++] = bson_iterator_double(sub);
+                }
+            }
+
+            printf("Input Vector = %f, %f, %f\n", tmp[0], tmp[1], tmp[2] );
+
+            p_internal[i] = (TrainingSetItem){ .input_vector = { tmp[0], tmp[1], tmp[2] }, .expected_output = bson_iterator_int(expected_output) };
+        }
+        i++;
+    }
+
+    bson_destroy( query );
+    mongo_cursor_destroy( cursor );
+    mongo_destroy(conn);
+
     return p_internal;
-    // Done
-    return 0;
 }
 
 int train(TrainingSetItem *ts, unsigned int len)
@@ -112,15 +146,11 @@ int train(TrainingSetItem *ts, unsigned int len)
 
             memcpy(input_vector, ts[i].input_vector, sizeof(ts[i].input_vector));
             printf("ts[%d] is { %f, %f, %f }\n", i, ts[i].input_vector[0], ts[i].input_vector[1], ts[i].input_vector[2]);
-            //printf("sizeof input_vector %lu\n", sizeof(input_vector));
-            //break;
 
             printf("input_vector = {%f, %f, %f}\n", input_vector[0], input_vector[1], input_vector[2]);
-            //input_vector = ts[i].input_vector;
 
-            //desired_output = ts[i].expected_output;
             memcpy(&desired_output, &ts[i].expected_output, sizeof(ts[i].expected_output));
-            //desired_output = ts[i].expected_output;
+
             printf("Expected outcome = %d\n", desired_output);
 
             product = dot_product(input_vector, weights, sizeof(input_vector)/sizeof(input_vector[0]));
@@ -130,14 +160,13 @@ int train(TrainingSetItem *ts, unsigned int len)
                 result = 0;
             }
             printf("Result %d\n", result);
-            // int = int - double
+
             error  = desired_output - result;
             printf("Error %d\n", error);
 
             if (error != 0) {
                 errCnt++;
 
-                //for(j = 0; j < sizeof(weights)/sizeof(weights[0]); j++) {
                 printf("Original weights {%f, %f, %f}\n", weights[0], weights[1], weights[2]);
                 for(j = 0; j < len_input_and_weights; j++) {
                     printf("   w[j] = %f\n", rate * error * input_vector[j]);
