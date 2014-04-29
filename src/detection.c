32d623a7980f2e5cecfc8d5553e1f73ec4a2dfda
Tomas Libal <tomas@libal.eu>
2014-04-29 19:30:54 +0200

    Renamed some structs, added a debug output definition

--- src/detection.c
+++ src/detection.c
@@ -7,14 +7,23 @@
 #include "ann.h"
 #include "dbh.h"
 
-// User-Agent string to work with
+#define APP_DEBUG 1
+
+#ifdef APP_DEBUG
+#define DEBUGPRINT(...) fprintf(stdout, __VA_ARGS__)
+#define PRNTSTR(val,name) \
+    printf("%s = %s\n", name, val);
+#else /* !APP_DEBUG */
+#define DEBUGPRINT(...)
+#define PRNTSTR(...)
+#endif /* !APP_DEBUG */
+
+/* User-Agent string to work with */
 char *uas;
-// E.g. android-devices
+/* E.g. android-devices */
 char *group_name;
-// Verbose the output
-int v = 1;
-// Used to determine if ANN is trained or not
-bool ann_trained = false;
+/* Verbose the output */
+short v = 1;
 
 // add_argument function will be used to handle the input arguments from the
 // command line so that they won't have to be specified in a given order by by
@@ -51,26 +60,31 @@ int main(int argc, char *argv[])
     if(v) printf("Testing the database handlers\n");
     dbh_test();
 
+    // Set the group-name
+    ann_set_group(group_name);
+
     // Load the training set and train the perceptron
-    TrainingSetItem *p_ts;
-    unsigned int len;
+    ann_training_set_t *p_ts;
+    unsigned int       len;
     p_ts = load_training_set_from_db(&len);
     if (p_ts == NULL || len == 0) {
         printf("Exiting here - empty training set\n");
         return 1;
     }
     train(p_ts, len);
+    free(p_ts);
 
-    // Parse the User-Agent and save the result in the ParsedUserAgent struct
-    static ParsedUserAgent uas_parsed;
-    ParsedUserAgent *puas_parsed;
+    // Parse the User-Agent and save the result in the ann_parsed_user_agent struct
+    ann_parsed_user_agent uas_parsed;
+    ann_parsed_user_agent *puas_parsed = NULL;
     puas_parsed = &uas_parsed;
+
     parse_user_agent(uas, puas_parsed);
 
-    // Run the ParsedUserAgent through the perceptron
+    // Run the ann_parsed_user_agent through the perceptron
     int result = run(puas_parsed);
     if(v) printf("ANN Result: %d = %s\n", result,(result>0)?"UAS is in the GRUP":"UAS is not in the GROUP");
-    free(p_ts);
+
 
     // Return results in JSON format to stdout
     // ...
