2d41b9348f52ff378343ea5373c3c8766e356f2c
Tomas Libal <tomas@libal.eu>
2014-04-29 19:34:00 +0200

    Started a utils header file which has a couple of macro definitions - notably for the debug output

---
+++ src/utils.h
@@ -0,0 +1,12 @@
+#ifndef DETECTION_UTILS
+#define APP_DEBUG 1
+
+#ifdef APP_DEBUG
+#define DEBUGPRINT(...) fprintf(stdout, __VA_ARGS__)
+#define PRNTSTR(val,name) \
+    printf("%s = %s\n", name, val);
+#else /* !APP_DEBUG */
+#define DEBUGPRINT(...)
+#define PRNTSTR(...)
+#endif /* !APP_DEBUG */
+#endif /* !DETECTION_UTILS */
