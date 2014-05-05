73ab6979930e09798d24522ed8e6b5b5631bf084
Tomas Libal <tomas@libal.eu>
2014-05-05 21:24:23 +0200

    Suite's header file added

---
+++ test/suite.h
@@ -0,0 +1,18 @@
+#ifndef SUITE
+#define SUITE
+
+#include <assert.h>
+#include <stdio.h>
+
+extern int num_tests;
+extern int p;
+extern int f;
+
+#define ASSERT(msg, ...) \
+    num_tests++; \
+    if(__VA_ARGS__) { fprintf(stdout, "."); p++; }else{ fprintf(stdout, "\nF: "); fprintf(stdout, "(%s:%d) %s\n", __FILE__, __LINE__, msg); }
+
+/* call this when all test done */
+void done(int n, int p);
+
+#endif
