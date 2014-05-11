11899544a8355357780d4f23dfbd2374d0a4e31b
Tomas Libal <tomas@libal.eu>
2014-05-11 15:01:46 +0100

    added tests for sample_std_dev function

--- test/test_utils.c
+++ test/test_utils.c
@@ -11,15 +11,23 @@ int p = 0;
 
 int main()
 {
-    printf("Executing test case 'test_ann'\n");
+    printf("Executing test case 'test_utils'\n");
 
     /* sample_mean test */
     double *w = calloc(3, sizeof(double));
     w[0] = 1.0;
     w[1] = 2.0;
     w[2] = 3.0;
-    double r = sample_mean(w, 3);
-    ASSERT("Expect the sample_mean = 2", r==2.0);
+    double r = sample_mean(w, 1);
+    ASSERT("Expect sample_mean = 1", r==1.0);
+
+    r = sample_mean(w, 3);
+    ASSERT("Expect sample_mean = 2", r==2.0);
+
+    /* sample_std_test */
+
+    double o = sample_std_dev(w, 3, r);
+    ASSERT("Expect sample_std_dev = 1", o==1.0);
 
     return done(num_tests, p);
 }
