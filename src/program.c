56b6f1c593f2f22a37d93c9d29d350aba4d01f90
Tomas Libal <tomas@libal.eu>
2014-08-07 19:43:03 +0100

    adding the description of the interface

---
+++ src/program.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+
+    /*
+     This program acts as an interface to the problems defined in the README.md:
+
+     1. Does a user-agent string belong to a pre-defined group?
+
+     2. What device could the user-agent be sent from?
+
+     For the first problem, you'd call this program with two parameters:
+     ./program -ua <some user-agent string> -group <some group>
+
+     For the second problem, you'd call this program with one parameter only:
+     ./program -ua <some user-agent string>
+
+     Optionally, you can specify which format of the output you want:
+     ./program ... -output application/json
+
+    */
+
+    return 0;
+}
