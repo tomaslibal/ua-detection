761e468ca366f6c1e693161448a56e933f903905
Tomas Libal <tomas@libal.eu>
2014-08-08 20:40:33 +0100

    added inline documentation; added basic sanitization to two user inputs

--- tools/data_api.js
+++ tools/data_api.js
@@ -1,3 +1,14 @@
+/*
+
+ This program creates a simple RESTful HTTP API for the user-agent data stored
+ in the MongoDB
+
+ Usage:
+
+     node data_api.js
+
+ April 2014
+ */
 var http        = require('http');
 var Router      = require('node-simple-router');
 var MongoClient = require('mongodb').MongoClient;
@@ -10,19 +21,35 @@ var stdHeadersAndCORS = {
     'Access-Control-Allow-Origin': '*'
 };
 
+/**
+ * Returns a JavaScript object literal. This object is the prototype of what's
+ * being sent serialized in the HTTP response.
+ *
+ * @param id
+ * @returns {{id: *, error: null, results: Array}}
+ */
 function getFreshOutput(id) {
     return {
-        id: id,
+        id: id || 0,
         error: null,
         results: []
     };
 };
 
+/**
+ * Returns a HTTP response with the number of documents in the given collection.
+ * The response is in a json format.
+ *
+ * @param String coll
+ * @param object res
+ */
 function getCnt(coll, res) {
-    MongoClient.connect(db_uri, function(err, db) {
-        if(err) return;
-        if(!db) return;
+    // Check the input first
+    if("string" !== typeof coll) return 1;
 
+    MongoClient.connect(db_uri, function(err, db) {
+        if(err) return 2;
+        if(!db) return 3;
         // Make sure the user input is safe
         db.collection(coll).count(function(err,cnt) {
             res.writeHead(200, stdHeadersAndCORS);
@@ -33,6 +60,16 @@ function getCnt(coll, res) {
 
 var output = getFreshOutput();
 
+/**
+ * Helper function which looks up documents in the given collection and returns
+ * them in a HTTP response body as json data.
+ *
+ * @param object res HTTP Response object
+ * @param object output This is a JS object that will be serialized and sent over
+ * @param String collection
+ * @param object query
+ * @param object options
+ */
 function outFromDb(res, output, collection, query, options) {
     MongoClient.connect(db_uri, function(err, db) {
         if(err) output.error = err;
@@ -52,6 +89,14 @@ function outFromDb(res, output, collection, query, options) {
         });
     });
 };
+
+/*
+ *
+ *
+ *  R O U T I N G
+ *
+ *
+ */
 
 router.get("/debug", function(req, res) {
     var output = { "api_status": "OK", "router": "OK"};
@@ -142,6 +187,14 @@ router.get("/groups/:id", function(req, res) {
 
     outFromDb(res, output, 'groups', query);
 });
+
+/*
+ *
+ *
+ *  H T T P   S E R V E R
+ *
+ *
+ */
 
 var server = http.createServer(router);
 server.listen(29105);
