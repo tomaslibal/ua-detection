61e5e9f335ba98398c7f10e73d6187b984db668f
Tomas Libal <tomas@libal.eu>
2015-03-08 14:48:57 +0000

    add reader test into the runner

--- test/main.c
+++ test/main.c
@@ -3,10 +3,12 @@
 
 #include "htable_int_test.h"
 #include "tokenizer_test.h"
+#include "reader_test.h"
 
 int main()
 {
     run_test_htable_int();
     run_test_tokenizer();
+    run_test_reader();
     return 0;
 }
