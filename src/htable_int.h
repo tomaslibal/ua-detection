1368a1c945d5fd436faa2d6df91a96230b532a24
Tomas Libal <tomas@libal.eu>
2015-03-08 10:08:45 +0000

    fix missing semicolon

--- src/htable_int.h
+++ src/htable_int.h
@@ -5,7 +5,7 @@ struct htable_int {
     char *name;
     int val;
     struct htable_int *next;
-}
+};
 
 struct htable_int *htable_int_create();
 void htable_int_set(struct htable_int *table, char *name, int val);
