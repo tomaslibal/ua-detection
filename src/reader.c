99eb047aa30b2d1046803cb09e11ca07d2413576
Tomas Libal <tomas@libal.eu>
2015-03-14 22:19:38 +0000

    remove unused code

--- src/reader.c
+++ src/reader.c
@@ -117,31 +117,3 @@ void uas_record_set(struct uas_record *uas_record, char *class, char *uas, struc
 
 	uas_record->next = next;
 }
-
-/*
-int main()
-{
-    FILE *fp = fopen("../data/user_agents.txt", "r");
-    if(fp == NULL) {
-        exit(EXIT_FAILURE);
-    }
-
-    // keeps the count of how many user-agents have been read
-    int counter = 0;
-    // data column reserved for future use
-    int n1;
-    // data column reserved for future use
-    int n2;
-    // buffer for the user-agent strings
-    char ua_string[256];
-    while(fscanf(fp, "%d %d %256[^\t\n]", &n1, &n2, &ua_string) == 3) {
-        printf("user-agent string = %s\n", ua_string);
-        counter++;
-    }
-
-    printf("Done\nRead %d user-agent values\n", counter);
-
-    fclose(fp);
-    return 0;
-}
-*/
