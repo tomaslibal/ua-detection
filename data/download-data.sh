1c1b232668260a0b727c0020b834f3447cf00c03
Tomas Libal <tomas@libal.eu>
2015-03-14 21:43:00 +0000

    add error message if curl download fails

--- data/download-data.sh
+++ data/download-data.sh
@@ -4,6 +4,8 @@ curl --output uas_with_class.txt -O http://libal.eu/eph/uastrings/sample1.txt
 
 if [ "$?" -eq 0 ]; then
 	echo "Downloaded data files";
+else
+    echo "Error downloading the data file";
 fi
 
 exit 0;
