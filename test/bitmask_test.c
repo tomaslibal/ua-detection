01f86c2dac42924275313227e38a7e2389083020
Tomas Libal <tomas@libal.eu>
2015-03-21 14:37:53 +0000

    test mask_is_set

--- test/bitmask_test.c
+++ test/bitmask_test.c
@@ -43,8 +43,24 @@ void test_mask_unset()
     assert(mask == 0);
 }
 
+void test_mask_is_set()
+{
+    unsigned short mask = 0;
+    const unsigned short FLAG_1 = 1;
+    const unsigned short FLAG_2 = 1 << 1;
+    const unsigned short FLAG_3 = 1 << 2;
+
+    mask_set(&mask, &FLAG_1);
+    mask_set(&mask, &FLAG_2);
+
+    assert(mask_is_set(&mask, &FLAG_1) > 0);
+
+    assert(mask_is_set(&mask, &FLAG_3) == 0);
+}
+
 void run_test_bitmask()
 {
     test_mask_set();
     test_mask_unset();
+    test_mask_is_set();
 }
