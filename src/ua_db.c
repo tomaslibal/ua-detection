30ac30747feb043c523145aef6f91222668cd580
Tomas Libal <tomas@libal.eu>
2015-03-27 22:53:10 +0000

    described the cli usage

--- src/ua_db.c
+++ src/ua_db.c
@@ -1,11 +1,23 @@
 /*
  * ua_db.c
  *
+ * ua_db is a program that stores, labels and retrieves user-agent string data.
+ *
  *  Created on: Mar 26, 2015
  *      Author: tmlbl
  */
 
-int main()
+int main(int argc, char **argv)
 {
+
+    /*
+     * Usage
+     *
+     *     ua_db add "Mozilla/5.0 (Linux; Android) Firefox/35.0"
+     *     ua_db get "Mozilla/5.0 ..."
+     *     ua_db add-label <uas-id> label1[, label2, ...]
+     *     ua_db remove-label <uas-id> label1[, label2, ...]
+     */
+
     return 0;
 }
