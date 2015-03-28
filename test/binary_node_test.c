f69b966a69660a25ef2095eecbd04bac80c3e954
Tomas Libal <tomas@libal.eu>
2015-03-28 23:39:32 +0000

    add an empty suite file

---
+++ test/binary_node_test.c
@@ -0,0 +1,26 @@
+/*
+ * binary_node_test.c
+ *
+ *  Created on: Mar 28, 2015
+ *      Author: tmlbl
+ */
+#include <assert.h>
+#include <stdio.h>
+
+#include "test_suite.h"
+#include "binary_node_test.h"
+
+/*
+ * Code under test
+ */
+#include "../src/binary_node.c"
+
+int i_bnode = 0;
+
+int run_test_bnode()
+{
+    return i_bnode;
+}
+
+#include
+
