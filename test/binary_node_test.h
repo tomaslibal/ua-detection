2fb4d84831ea5802f4360380e5d0c43a16440e96
Tomas Libal <tomas@libal.eu>
2015-03-28 23:37:23 +0000

    add the header file

---
+++ test/binary_node_test.h
@@ -0,0 +1,13 @@
+/*
+ * binary_node_test.h
+ *
+ *  Created on: Mar 28, 2015
+ *      Author: tmlbl
+ */
+
+#ifndef TEST_BINARY_NODE_TEST_H_
+#define TEST_BINARY_NODE_TEST_H_
+
+void run_test_bnode();
+
+#endif /* TEST_BINARY_NODE_TEST_H_ */
