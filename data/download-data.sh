a646c678f5203e3dfa824d9fe3772e01b852df85
Tomas Libal <tomas@libal.eu>
2015-04-11 18:21:44 +0100

    download sample1.txt as mobile.cls.txt

--- data/download-data.sh
+++ data/download-data.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl --output uas_with_class.txt -O http://libal.eu/eph/uastrings/sample1.txt 
+curl --output mobile.cls.txt -O http://libal.eu/eph/uastrings/sample1.txt 
 
 if [ "$?" -eq 0 ]; then
 	echo "Downloaded data files";
