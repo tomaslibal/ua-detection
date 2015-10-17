23aa45452a0473bc6891dce598c9a7a6256ea231
Tomas Libal <tomas.libal@unrulymedia.com>
2015-10-17 22:59:33 +0100

    add Utils::in_array function

---
+++ src/util/utils.h
@@ -0,0 +1,18 @@
+/* 
+ * File:   utils.h
+ * Author: tomaslibal
+ *
+ * Created on 17 October 2015, 22:18
+ */
+
+#ifndef UTILS_H
+#define	UTILS_H
+
+namespace Utils {
+    
+    bool in_array(char* array, char ch);
+    
+}
+
+#endif	/* UTILS_H */
+
