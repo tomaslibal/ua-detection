91a8759aa999908669af388b8449582e5cea1f41
Tomas Libal <tomas@libal.eu>
2015-12-27 20:38:57 +0000

    add main's header

---
+++ common/src/uadet2.h
@@ -0,0 +1,39 @@
+/* 
+ * File:   uadet2.h
+ * Author: tlibal
+ *
+ * Created on December 27, 2015, 8:33 PM
+ */
+
+#ifndef UADET2_H
+#define	UADET2_H
+
+#include <string>
+#include <vector>
+#include <functional>
+
+/*
+ * Command line arguments are read and stored in a UadetSettings struct.
+ */
+struct UadetSettings {
+    std::string ua;
+    std::string category;
+};
+
+/*
+ * Function foreach applies the callback function to each element of the vector
+ */
+void foreach(std::vector<std::string>* vec, std::function<void (std::string)> &callback);
+
+/*
+ * The common stdout helper that prints a summary of available command line
+ * argumets for the program.
+ */
+void print_usage();
+
+void parse_args(int argc, char** argv, UadetSettings& settings);
+
+
+
+#endif	/* UADET2_H */
+
