75786da3b840dfb286abb14de0f66320ea5f7250
Tomas Libal <tomas@libal.eu>
2015-12-28 12:56:22 +0000

    implement test for staticTokenize

--- UATokenizer/test/uaTokenizerTest.cpp
+++ UATokenizer/test/uaTokenizerTest.cpp
@@ -7,6 +7,8 @@
 
 #include "uaTokenizerTest.h"
 
+#include <string>
+#include <vector>
 
 CPPUNIT_TEST_SUITE_REGISTRATION(uaTokenizerTest);
 
@@ -25,7 +27,19 @@ void uaTokenizerTest::tearDown() {
 }
 
 void uaTokenizerTest::testStaticTokenizeBreaksUpSentenceIntoTokens() {
-    CPPUNIT_ASSERT(true);
+    std::vector<std::string> tokens;
+    std::string sentence = "Subtracting equation (53) from equation (52) gives 8";
+    tok->staticTokenize(sentence, &tokens);
+    
+    CPPUNIT_ASSERT_MESSAGE("Tokens.size()=8", 8 == tokens.size());
+    CPPUNIT_ASSERT_MESSAGE("Tokens[0]=Subtracting", "Subtracting" == tokens[0]);
+    CPPUNIT_ASSERT_MESSAGE("Tokens[1]=equation", "equation" == tokens[1]);
+    CPPUNIT_ASSERT_MESSAGE("Tokens[2]=53", "53" == tokens[2]);
+    CPPUNIT_ASSERT_MESSAGE("Tokens[3]=from", "from" == tokens[3]);
+    CPPUNIT_ASSERT_MESSAGE("Tokens[4]=equation", "equation" == tokens[4]);
+    CPPUNIT_ASSERT_MESSAGE("Tokens[5]=52", "52" == tokens[5]);
+    CPPUNIT_ASSERT_MESSAGE("Tokens[6]=gives", "gives" == tokens[6]);
+    CPPUNIT_ASSERT_MESSAGE("Tokens[7]=8", "8" == tokens[7]);
 }
 
 
