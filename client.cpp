232d0e89cbb1ff390fb39728de954cb1f076fb85
Tomas Libal <tomas@libal.eu>
2016-01-04 20:54:08 +0000

    add the client main file

---
+++ client.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   client.cpp
+ * Author: tlibal
+ *
+ * Created on January 4, 2016, 8:51 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
