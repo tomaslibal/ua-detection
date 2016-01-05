8333e03d6bba6a2d22e255d5eff8d1504579a223
Tomas Libal <tomas@libal.eu>
2016-01-05 20:10:14 +0000

    add the server's main file'

---
+++ server.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   server.cpp
+ * Author: tlibal
+ *
+ * Created on January 5, 2016, 8:08 PM
+ */
+
+#include <cstdlib>
+
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
