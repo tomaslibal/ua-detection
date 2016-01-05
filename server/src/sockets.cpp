07f08a9184a80f0c830b2e68636255aeca61b8d8
Tomas Libal <tomas@libal.eu>
2016-01-05 20:39:20 +0000

    add a sample socket comm. code

---
+++ server/src/sockets.cpp
@@ -0,0 +1,49 @@
+#include "sockets.h"
+#include "utils.h"
+#include "protocol.h"
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using std::function;
+using std::mutex;
+using std::unique_lock;
+
+int create_socket_inet_stream() {
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        error("ERROR Opening Socket");
+    }
+    return sockfd;
+}
+
+void wait_and_accept(sockaddr_in* cli_addr, int insockfd, function<void ()>& exit_callback, unique_lock<mutex>& signal_exit) {
+    socklen_t clilen;
+    char buffer[256];
+    int n;
+    
+    cout << "Connection established (" << insockfd << ")" << endl;
+
+    bzero(buffer,256);
+    n = read(insockfd,buffer,255);
+    if (n < 0) error("ERROR reading from socket");
+    string* output = process_message(buffer);   
+    cout << "message out is " << *output;
+    
+    if (strcmp(buffer, "exit\n") == 0) {
+        signal_exit.try_lock();
+        delete output;
+        close(insockfd);
+        exit_callback();
+        return;
+    }
+    
+    n = write(insockfd, output->data(), output->length());
+    if (n < 0) error("ERROR writing to socket");
+    close(insockfd);
+      
+    bzero(buffer, 256);
+    delete output;
+    return;
+}
---
+++ server/src/sockets.h
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   sockets.h
+ * Author: tlibal
+ *
+ * Created on January 5, 2016, 8:32 PM
+ */
+
+#ifndef SOCKETS_H
+#define SOCKETS_H
+
+#include <functional>
+#include <mutex>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int create_socket_inet_stream();
+
+void wait_and_accept(
+    sockaddr_in* cli_addr, 
+    int insockfd, 
+    std::function<void ()>& exit_callback, 
+    std::unique_lock<std::mutex>& signal_exit
+);
+
+#endif /* SOCKETS_H */
+
