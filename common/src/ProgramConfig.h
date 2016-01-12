2514a156b0b96ea057ae37efc158bdcd76e42146
Tomas Libal <tomas@libal.eu>
2016-01-12 20:03:09 +0000

    update header comment

--- common/src/ProgramConfig.h
+++ common/src/ProgramConfig.h
@@ -1,10 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
+ * This class reads the config file and sets appropriate variables to the values
+ * from the configuration.
+ *  
  * File:   ProgramConfig.h
  * Author: tlibal
  *
