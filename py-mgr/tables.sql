f79b74232e6cf5392925002a6884e29946c369df
Tomas Libal <tomas@libal.eu>
2016-01-23 12:43:13 +0000

    make IDs type serial
    - this makes the id column autoincrement integers

--- py-mgr/tables.sql
+++ py-mgr/tables.sql
@@ -1,15 +1,16 @@
 CREATE TABLE IF NOT EXISTS datapoints (
-    id integer NOT NULL PRIMARY KEY,
+    id serial PRIMARY KEY,
     value varchar(512)
 );
 
 CREATE TABLE IF NOT EXISTS labels (
-    id integer NOT NULL PRIMARY KEY,
+    id serial PRIMARY KEY,
     value varchar(128)
 );
 
 CREATE TABLE IF NOT EXISTS datapoint_labels (
-    id integer NOT NULL PRIMARY KEY,
+    id serial PRIMARY KEY,
+    datapoint_id integer NOT NULL,
     label_id integer NOT NULL,
     -- confidence is a floating point number in the interval [0, 1] 
     -- representing the confidence with which the label is assigned to the 
