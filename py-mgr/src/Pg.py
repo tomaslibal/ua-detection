3f95c3ef5974556a07ef228876648560ca4f719f
Tomas Libal <tomas@libal.eu>
2016-01-23 11:53:35 +0000

    use .format for the query

--- py-mgr/src/Pg.py
+++ py-mgr/src/Pg.py
@@ -2,13 +2,18 @@ import psycopg2
 
 class Pg:
     dbname = 'uadet'
-    user = 'postgres'
+    user = 'client'
+    pwd = 'client'
     conn = None
     def __init__(self):
-        self.conn = psycopg2.connect("dbname=" + self.dbname + " user=" + self.user)
+        self.conn = psycopg2.connect(dbname=self.dbname, user=self.user, password=self.pwd, host="127.0.0.1")
     def list_table(self, table_name, offset, limit):
-        cur = self.conn.execute("SELECT * FROM %s LIMIT %s OFFSET %s", (table_name, limit, offset))
-        cur.fetchone()
+        cur = self.conn.cursor()
+        query = 'SELECT * FROM "{}" LIMIT {} OFFSET {}'.format(table_name, limit, offset)
+        cur.execute(query)
+        for record in cur:
+            print record
         cur.close()
+        return cur
     def __exit__(self, exc_type, exc_value, traceback):
         self.conn.close()
