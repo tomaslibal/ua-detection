7661a67d20d999a4fb8ebcc212830b75461f9c1f
Tomas Libal <tomas@libal.eu>
2016-01-24 14:25:45 +0000

    instantiate 2 tables on init: datapoints and labels

--- py-mgr/js-src/main.js
+++ py-mgr/js-src/main.js
@@ -1,2 +1,14 @@
 import TableModel from "./TableModel";
 import TableRowModel from "./TableRowModel";
+import { TableController } from "./TableController";
+
+var tableInstances = ['datapoints', 'labels'];
+
+tableInstances
+    .map(tableName => {
+        return new TableController(document.querySelector(`#${tableName}Table`), tableName)
+    })
+    .forEach(tableInstance => {
+        tableInstance.populate()
+    });
+
