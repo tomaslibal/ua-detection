dd8f228358c30effd34b0aa5fd3d48c3f90ced47
Tomas Libal <tomas@libal.eu>
2016-01-27 13:29:18 +0000

    merge the table tests into one file

--- py-mgr/js-test/tableTest.js
+++ py-mgr/js-test/tableTest.js
@@ -1,4 +1,4 @@
-import TableModel from '../js-src/TableModel';
+import TableModel from '../js-src/Table';
 
 let chai = require('chai');
 let assert = require('assert');
@@ -39,4 +39,32 @@ describe('TableModel', () => {
     
 });
 
+describe('TableRowModel', () => {
 
+   let row;
+
+   beforeEach('setup the s.u.t', () => {
+      row = new TableRowModel();
+   });
+    
+   it('inits with 0 columns', () => {
+       chai.assert.deepEqual(row.columns, []);
+   });
+
+   it('computes number of columns as columns.length', () => {
+       row.columns = [1,2,3];
+
+       chai.assert.equal(row.getNumColumns(), 3);
+   });
+
+   it('adds a cell to the list of cells (columns)', () => {
+        row.addColumn('foo');
+
+        chai.assert.deepEqual(row.columns, ['foo']);
+
+        row.addColumn('bar');
+
+        chai.assert.deepEqual(row.columns, ['foo', 'bar']);
+   });
+
+});
