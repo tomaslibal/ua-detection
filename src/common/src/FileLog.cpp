87b3eb55aaea357c8576f9d7586a2bf6dcf4806f
Tomas Libal <tomas@libal.eu>
2016-02-04 13:57:45 +0000

    write a message as a log line into a file

--- src/common/src/FileLog.cpp
+++ src/common/src/FileLog.cpp
@@ -1,5 +1,9 @@
 #include "FileLog.h"
 
+#include <fstream>
+#include <ctime>
+#include <string>
+
 FileLog::FileLog()
 {
 
@@ -17,5 +21,22 @@ FileLog::~FileLog()
 
 void FileLog::setPath(const std::string& path)
 {
-    this.path = path;
+    this->path = path;
+}
+
+void FileLog::log(const std::string& msg)
+{
+	std::ofstream ofile(path, std::fstream::app);
+
+	if (!ofile.good()) {
+		return;
+	}
+
+    std::time_t now = std::time(nullptr);
+	char timestamp[60];
+	std::strftime(timestamp, sizeof(timestamp), "%c ", std::gmtime(&now));
+	
+	std::string logLine = std::string(timestamp) + msg + "\n";
+
+	ofile.write(logLine.c_str(), logLine.length());
 }
