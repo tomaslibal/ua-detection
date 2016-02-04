b7a99b9123ab5bdead0ffd9e48285224617e74a5
Tomas Libal <tomas@libal.eu>
2016-02-04 13:37:47 +0000

    fix a missing semicolon and add a  member function

--- src/common/src/FileLog.h
+++ src/common/src/FileLog.h
@@ -11,8 +11,9 @@ public:
 	virtual ~FileLog();
 
     void setPath(const std::string& path);
+	void log(const std::string& msg);
 private:
-    std::string path = "./uadet2.log.txt"
+    std::string path = "./uadet2.log.txt";
 };
 
 #endif /* FILELOG_H */
