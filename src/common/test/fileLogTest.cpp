9fd192dd2d3eadc1cd5f021f183a71956afd8741
Tomas Libal <tomas@libal.eu>
2016-02-05 13:36:36 +0000

    add an empty test suite for FileLog class

---
+++ src/common/test/fileLogTest.cpp
@@ -0,0 +1,33 @@
+/*
+ * File:   fileLogTest.cpp
+ * Author: tlibal
+ *
+ * Created on Feb 5, 2016 
+ */
+
+#include "fileLogTest.h"
+#include <iostream>
+
+CPPUNIT_TEST_SUITE_REGISTRATION(fileLogTest);
+
+fileLogTest::fileLogTest()
+{
+}
+
+fileLogTest::~fileLogTest()
+{
+}
+
+void fileLogTest::setUp()
+{
+    logger = new Logger;
+}
+
+void fileLogTest::tearDown()
+{
+    delete logger;
+}
+
+void fileLogTest::testFoo()
+{
+}
---
+++ src/common/test/fileLogTest.h
@@ -0,0 +1,34 @@
+/*
+ * File:   fileLogTest.h
+ * Author: tlibal
+ *
+ * Created on Feb 5, 2016
+ */
+
+#ifndef FILELOGTEST_H
+#define	FILELOGTEST_H
+
+#include <cppunit/extensions/HelperMacros.h>
+
+#include "../src/FileLog.h"
+
+class fileLogTest: public CPPUNIT_NS::TestFixture {
+    CPPUNIT_TEST_SUITE(fileLogTest);
+
+    CPPUNIT_TEST(testFoo);
+    
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+    fileLogTest();
+    virtual ~fileLogTest();
+    void setUp();
+    void tearDown();
+
+private:
+    FileLog logger;
+    void testFoo();
+};
+
+#endif	/* FILELOGTEST_H */
+
