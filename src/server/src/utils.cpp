fbb812d39d2025808035fcde12755f55084ff583
Tomas Libal <tomas@libal.eu>
2016-02-14 15:21:48 +0000

    add get_percentile() func

--- src/server/src/utils.cpp
+++ src/server/src/utils.cpp
@@ -2,12 +2,31 @@
 
 #include <string>
 #include <iostream>
+#include <cmath>
 
 using std::string;
 using std::cerr;
 using std::endl;
 
-void error(string msg) {
-	perror(msg.data());
+void error(string msg)
+{
+    perror(msg.data());
     exit(EXIT_FAILURE);
+}
+
+double get_percentile(double value, std::map<double, std::string>& population)
+{
+    int size = population.size();
+    int num_higher_values = 0;
+    int num_smaller_or_equal_values = 0;
+    
+    for (auto& item: population) {    
+	if (item.first <= value) {
+	  num_smaller_or_equal_values++;
+	}
+    }
+    
+    num_higher_values = size - num_smaller_or_equal_values;
+    
+    return std::round( num_smaller_or_equal_values / size );
 }
--- src/server/src/utils.h
+++ src/server/src/utils.h
@@ -15,8 +15,19 @@
 #define SERVER_H
 
 #include <string>
+#include <map>
 
 void error(std::string msg);
+
+/**
+ * Computes in which percentile the value (roughly) is. The result is rounded to the nearest unit.
+ * 
+ * @param double Value for which the percentile is calculated
+ * @param std::map<double, std::string>& Data used to determine which percentile the first parametr is in
+ * 
+ * @return double Number between 0 and 100 (inclusive 0 and 100)
+ */
+double get_percentile(double value, std::map<double, std::string>& population);
 
 #endif /* SERVER_H */
 
