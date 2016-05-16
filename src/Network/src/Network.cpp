05c0be880c06ce4fd0b5e5144e1f66346c4c049e
Tomas Libal <tomas.libal@unrulymedia.com>
2016-05-16 08:48:21 +0100

    adds open_connection method

--- src/Network/src/Network.cpp
+++ src/Network/src/Network.cpp
@@ -1,5 +1,13 @@
 #include "Network.h"
 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <string>
+#include <cstring>
+
 Network::Network()
 {
 }
@@ -16,4 +24,35 @@ void Network::set_port_no(int portno)
 int Network::get_port_no()
 {
   return portno;
+}
+
+int Network::open_connection(struct hostent *host)
+{
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  
+/*  if (sockfd < 0) 
+  {
+    logger.log("ERROR opening socket");
+    error("ERROR opening socket");
+  }
+
+  if (server == NULL)
+  {
+    logger.log("ERROR, no such host");
+    error("ERROR, no such host");
+  }
+  
+  */
+
+  bzero((char *) serv_addr, sizeof(serv_addr));
+  serv_addr->sin_family = AF_INET;
+  bcopy((char *)host->h_addr, (char *)serv_addr->sin_addr.s_addr, host->h_length);
+  serv_addr->sin_port = htons(portno);
+
+  if (::connect(sockfd,(const sockaddr*) serv_addr,sizeof(serv_addr)) < 0) {        
+ //   logger.log("ERROR connecting");
+ //   error("ERROR connecting");
+  }
+    
+  return sockfd;
 }
--- src/Network/src/Network.h
+++ src/Network/src/Network.h
@@ -8,10 +8,15 @@ class Network {
 
   void set_port_no(int portno);
   int  get_port_no();
+  
+  /**
+   * opens tcp connection, return the socket file descriptor
+   */
+  int  open_connection(struct hostent *host);
  private:
   int sockfd;
   int portno;
-  struct sockaddr_in serv_addr;
+  struct sockaddr_in *serv_addr;
   struct hostent *server;
 };
 
