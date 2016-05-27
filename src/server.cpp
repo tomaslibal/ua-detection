d5c63d6a378fbeb0b60b49f2c8a2578795cda0a3
Tomas Libal <tomas.libal@unrulymedia.com>
2016-05-27 19:49:46 +0100

    use Server class instead of the procedural structure

--- src/server.cpp
+++ src/server.cpp
@@ -9,194 +9,41 @@
 
 #include <cstdlib>
 #include <iostream>
-#include <thread>
-#include <string>
-#include <functional>
-#include <chrono>
-#include <mutex>
-#include <cstring>
 #include <csignal>
-#include <vector>
-#include <fstream>
-#include <sstream>
 
-#include "Server/src/utils.h"
-#include "Server/src/sockets.h"
-
-#include "FileInputReader/src/FileInputReader.h"
-#include "NaiveBayessClassifier/src/NaiveBayessClassifier.h"
-#include "common/src/uadet2.h"
-#include "common/src/ProgramConfig.h"
 #include "common/src/FileLog.h"
-#include "Network/src/Network.h"
-
-using std::function;
-using std::cout;
-using std::cerr;
-using std::endl;
-using std::mutex;
-using std::thread;
-using std::string;
-
-/**
- * Static socket filedescriptor variable so that it can be referenced from the signal handler function.
- * 
- * @param int sockfd
- */
-static int sockfd;
+#include "Server/src/Server.h"
 
 /*
  * 
  */
 int main(int argc, char** argv) {
-    int insockfd;
-    int portno;
-    sockaddr_in serv_addr, cli_addr;
     
-    socklen_t clilen;
-    clilen = sizeof(cli_addr);
-
+    /*
+     * Server module
+     */
+    Server server;
+    
     FileLog logger;
+    
+    server.set_logger(logger);
+    
     logger.log("Initializing Server");
 
-    /*
-     * Get the program config
-     */
-    string serverConfigFile = "src/common/config/server.txt";
-    ProgramConfigObject conf;
-    ProgramConfig confCtrl(serverConfigFile);
-
-    confCtrl.update(conf);
-    logger.log("Config file read and updated");
-
-    char buffer[256];
-    Network network;
     
     /*
      * catch the ctrl+c interrupt signal
      */
-    signal(SIGINT, [] (int signum) {
-        close(sockfd);
+    signal(SIGINT, [] (int signum) {        
+        if (Server::sockfd >= 0) {
+            close(Server::sockfd);
+        }
         exit(signum);
     });
     
-    /*
-     * Start the learning phase: open the data file and add the data to the naive
-     * bayess classifier
-     */
-    /*
-     * The program uses a Naive Bayess Classifier to predict a posterior of 
-     * a given user-agent string.
-     */
-    NaiveBayessClassifier nb;
+    server.learn();
     
-    /*
-     * The learning data file
-     */
-    string dataFile = conf.datafile;
-
-    logger.log("Using " + conf.datafile + " datafile");
-
-    /* 
-     * A lambda function that takes a string line. It expects the line to have
-     * two columns: categories and a user-agent string, separated by a tab
-     * character.
-     * The function then calls NaiveBayesClassifier.add_data to add the new 
-     * user agent string with its category(ies) to the memory.
-     */
-    function<void (string)> add_line = [&nb](string line) {
-        string category, uas;
-        string::size_type n = line.find('\t');
-        std::vector<std::string> categories;
-        
-        if (n != string::npos) {
-            category = line.substr(0, n);
-            uas = line.substr(n+1);
-
-            strsplit(category, ',', categories);
-            if (categories.size() > 1) {
-                for(std::vector<std::string>::iterator it = categories.begin(); it != categories.end(); ++it) {
-                    nb.add_data(uas, *it);
-                }
-            } else {
-                nb.add_data(uas, category);
-            }
-        }
-    };
-    
-    /*
-     * FileInputReader for reading in the data file, and passing each file line
-     * to the previous lambda.
-     */
-    FileInputReader reader;
-    
-    logger.log("Start: reading the datafile");
-    
-    reader.readLines(dataFile, add_line);
-    
-    logger.log("Finish: reading the datafile");
-    
-    /*
-     * Prints summary of number of categories and n-grams that have been read.
-     */
-    nb.stats();
-    
-    /*
-     * Start the network server:
-     */
-    
-    network.set_port_no(conf.portno);
-    
-    sockfd = network.addr_listen();
-    
-    logger.log("Using port number " + std::to_string(portno));
-    
-    logger.log("Now listening to incoming connections");
-
-    mutex signal_exit;
-    std::unique_lock<mutex> lck (signal_exit);
-    lck.unlock();
-    
-    function<void ()> exitCallback = []() {
-        cout << "exiting now..." << endl;
-        close(sockfd);
-        exit(EXIT_SUCCESS);
-    };
-    
-    /* 
-     * Worker is started as a new thread, it processes the incoming request
-     * in a detached state so it should not block other requests.
-     */
-    function<void (int)> worker2 = [&exitCallback, &lck, &nb](int in_sockfd) {
-        evaluate_incoming_request(in_sockfd, exitCallback, lck, nb);
-    };
-    
-    while(1) {
-        /*
-         * worker2 calls wait_and_accept (in sockets.h) which may lock the unique
-         * lock 'lck' if it receives a signal to exit. This lock will interrupt
-         * this while loop.
-         */
-        if (lck) {
-            cout << "mutex locked, must exit" << endl;
-            break;
-        }
-        /*
-         * waits until an incoming connection is made
-         */
-	logger.log("Waiting for a connection");
-	
-        int in_sockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-
-        if (in_sockfd < 0) { perror("in_sockfd < 0"); }
-        
-        logger.log("Incoming connection...Creating a new thread to handle it");
- 
-        thread a_thread (worker2, in_sockfd);
-        a_thread.detach();
-    }
-
-    close(sockfd);
+    server.start();
     
     return 0;
 }
