69667b024901b25cb54ef73e460abb5fc46fe8a1
Tomas Libal <tomas.libal@unrulymedia.com>
2016-05-31 08:37:49 +0100

    normalize the use of const in this file

--- src/Server/src/Server.h
+++ src/Server/src/Server.h
@@ -16,8 +16,8 @@ public:
     
     static int sockfd;
     
-    void set_logger(const FileLog& logger);
-    void set_config(const ProgramConfigObject& config);
+    void set_logger(FileLog const& logger);
+    void set_config(ProgramConfigObject const& config);
     void learn();
     void start();
     void stop();
@@ -26,12 +26,22 @@ private:
     ProgramConfigObject config;
     NaiveBayessClassifier nb;
     
+    /*
+     * Incremental counter for each new incoming connection
+     */
     std::atomic<int> nIncomingMsg{0};
     
     const std::string configFilePath = "config/server.txt";
     
+    /*
+     * Each new connection is handled in its own thread
+     */
     void create_thread();
-    void log(const std::string& msg);
+    
+    /*
+     * Writes the message to the log
+     */
+    void log(std::string const& msg);
 };
 
 #endif /* _SERVER_H_ */
