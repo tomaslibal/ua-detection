dc6005885d52fc965bd8b25cb2d7255bb0b5556b
Tomas Libal <tomas@libal.eu>
2016-06-28 23:42:13 +0100

    add a header file for the command class

---
+++ src/common/src/Command.h
@@ -0,0 +1,48 @@
+#ifndef COMMAND_H
+#define COMMAND_H
+
+#include <string>
+
+enum class CommandSource { Server, Client };
+
+/**
+ * uadet client and server send commands between each other over the TCP 
+ * connection. This class characterizes the abstraction of these commands
+ * which are send as ascii text.
+ * 
+ * Example: a client sends the following message
+ * 
+ *     eval Mozilla/5.0 (Linux; Android EN) Firefox/44.0
+ * 
+ * When this message becomes a Command instance it will have these fields:
+ * 
+ *     source=CommandSource.Client
+ *     name=eval
+ *     data=Mozilla/5.0 (Linux; Android EN) Firefox/44.0
+ *
+ */
+class Command
+{
+public:
+    Command(CommandSource const commandSource, std::string const& name, std::string const& data) : commandSource(commandSource), name(name), data(data) {};
+    virtual ~Command();
+    
+private:
+    CommandSource const commandSource;
+    std::string const name;
+    std::string const data;
+};
+
+class CommandBuilder
+{
+public:
+    CommandBuilder();
+    virtual ~CommandBuilder();
+    
+    void withCommandSource(CommandSource const commandSource);
+    void withName(std::string const& name);
+    void withData(std::string const& data);
+    Command build();
+};
+
+#endif /* COMMAND_H */
