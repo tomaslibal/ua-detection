631962ff9466772ae96add7720a31a2730402e20
Tomas Libal <tomas.libal@unrulymedia.com>
2016-07-01 11:10:27 +0100

    add command test

---
+++ src/common/test/commandTest.cpp
@@ -0,0 +1,48 @@
+/*
+ * File:   commandTest.cpp
+ * Author: tlibal
+ *
+ * Created on Jul 1, 2016
+ */
+
+#include "commandTest.h"
+
+#include <iostream>
+
+CPPUNIT_TEST_SUITE_REGISTRATION(commandTest);
+
+commandTest::commandTest()
+{
+}
+
+commandTest::~commandTest()
+{
+}
+
+void commandTest::setUp()
+{
+    
+}
+
+void commandTest::tearDown()
+{
+    
+}
+
+void commandTest::testBuilder()
+{
+    CommandBuilder cmdBuilder;
+    
+    cmdBuilder.start()
+        ->withCommandSource(CommandSource::Client)
+        ->withName("eval")
+        ->withData("Mozilla/5.0 (Linux; Android 6; en) Firefox/45.0");
+    
+    Command cmd = cmdBuilder.build();
+    
+    CPPUNIT_ASSERT(cmd.getCommandSource() == CommandSource::Client);
+    CPPUNIT_ASSERT(cmd.getName().compare("eval") == 0);
+    CPPUNIT_ASSERT(cmd.getData().compare("Mozilla/5.0 (Linux; Android 6; en) Firefox/45.0") == 0);
+}
+
+
---
+++ src/common/test/commandTest.h
@@ -0,0 +1,33 @@
+/*
+ * File:   commandTest.h
+ * Author: tlibal
+ *
+ * Created on Feb 5, 2016
+ */
+
+#ifndef COMMANDTEST_H
+#define	COMMANDTEST_H
+
+#include <cppunit/extensions/HelperMacros.h>
+
+#include "../src/Command.h"
+
+class commandTest: public CPPUNIT_NS::TestFixture {
+    CPPUNIT_TEST_SUITE(commandTest);
+
+    CPPUNIT_TEST(testBuilder);
+    
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+    commandTest();
+    virtual ~commandTest();
+    void setUp();
+    void tearDown();
+
+private:
+    void testBuilder();
+};
+
+#endif	/* COMMANDTEST_H */
+
