1f8f7161bedabaa18c5a2da2d2a3d608d878e169
Tomas Libal <tomas@libal.eu>
2016-12-19 23:55:17 +0800

    add test fixture for Server class

---
+++ src/Server/test/serverTest.cpp
@@ -0,0 +1,37 @@
+/*
+ * File:   serverTest.cpp
+ * Author: tlibal
+ *
+ * Created on Dec 29, 2015, 4:41:06 PM
+ */
+
+#include "serverTest.h"
+
+#include <vector>
+#include <string>
+
+CPPUNIT_TEST_SUITE_REGISTRATION(serverTest);
+
+serverTest::serverTest() {
+}
+
+serverTest::~serverTest() {
+}
+
+void serverTest::setUp() {
+    svr = new Server();
+}
+
+void serverTest::tearDown() {
+    delete svr;
+}
+
+void serverTest::testJsonOutput()
+{
+   std::vector<std::string> labels;
+   std::vector<double> values;
+
+   std::string* actual = svr->json_output(values, labels);
+
+   delete actual;
+}
---
+++ src/Server/test/serverTest.h
@@ -0,0 +1,34 @@
+/*
+ * File:   serverTest.h
+ * Author: tlibal
+ *
+ * Created on Dec 29, 2015, 4:41:05 PM
+ */
+
+#ifndef serverTest_H
+#define	serverTest_H
+
+#include <cppunit/extensions/HelperMacros.h>
+
+#include "../src/Server.h"
+
+class serverTest : public CPPUNIT_NS::TestFixture {
+    CPPUNIT_TEST_SUITE(serverTest);
+
+    CPPUNIT_TEST(testJsonOutput);
+  
+    CPPUNIT_TEST_SUITE_END();
+
+public:
+    serverTest();
+    virtual ~serverTest();
+    void setUp();
+    void tearDown();
+
+private:
+    Server* svr;
+    void testJsonOutput();
+};
+
+#endif	/* serverTest_H */
+
